98233dca46638a4cb4c2cb5743b0cc0afa246d8a
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2016-10-06 19:23:33 +0300

    initial commit

---
+++ controllers/CatalogController.php
@@ -0,0 +1,21 @@
+<?php
+
+include_once ROOT . '/models/Category.php';
+include_once ROOT . '/models/Product.php';
+
+class CatalogController {
+    public function actionIndex() {
+        $categories = Category::getCategoriesList();
+        $latestProducts = Product::getLatestProducts(12);
+        require_once(ROOT . '/views/catalog/index.php');
+        return true;
+    }
+    
+    public function actionCategory($categoryId) {
+        $categories = Category::getCategoriesList();
+        $categoryProducts = Product::getProductsListByCategory($categoryId);
+        require_once(ROOT . '/views/catalog/category.php');
+        return true;
+    }
+}
+
---
+++ controllers/SiteController.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once ROOT . '/models/Category.php';
+include_once ROOT . '/models/Product.php';
+
+class SiteController {
+    public function actionIndex() {
+        $categories = Category::getCategoriesList();
+        $latestProducts = Product::getLatestProducts(12);
+        require_once(ROOT . '/views/site/index.php');
+        return true;
+    }
+}
+
